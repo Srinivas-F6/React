ab04bccd7b4f56c2200f7fec0d51bc3bad08ff29
_Srinivas_ <glsrinivas1819@gmail.com>
2025-10-10 14:54:53 +0530

    RTK

--- RTK-query/src/App.jsx
+++ RTK-query/src/App.jsx
@@ -1,34 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useGetProductsQuery } from './store/api'
+import { api } from './store/api'
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const { data, error, isLoading } = useGetProductsQuery('products')
+  console.log(1);
+  if (isLoading) return <h2>Loading....</h2>
+  if (error) return <h2>{error}</h2>
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <ul>
+      {
+          data?.products?.map((product, idx)=>(
+            <li key={idx}>{product.title}<br/> {product.category}<br/><br/></li>
+          ))
+      }
+    </ul>
   )
 }
 
--- RTK-query/src/store/api.jsx
+++ RTK-query/src/store/api.jsx
@@ -1,12 +1,23 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 
-
-const api = createApi({
+console.log(3);
+export const api = createApi({
     reducerPath: "contactsApi",
     baseQuery: fetchBaseQuery({
-        baseUrl:"link"
+        baseUrl:"https://dummyjson.com"
     }),
     endpoints:(builder)=>({
-        
+        getProducts: builder.query({
+            query:(p)=>`/${p}`
+        })
+        // addProducts: builder.mutation({
+        //     query: (product)=>({
+        //         url:'/users',
+        //         method:'POST',
+        //         product
+        //     })
+        // })
+      
     })
 })
+export const{useGetProductsQuery} = api;
